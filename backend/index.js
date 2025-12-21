6ab2a24877765f397375c5a030df5f98a84d8225
jaswanthinvalley <jaswanthvenkat001@gmail.com>
2025-12-21 13:00:52 +0530

    jwt implemented

--- backend/index.js
+++ backend/index.js
@@ -4,10 +4,13 @@ const dotenv = require('dotenv')
 dotenv.config()
 const port = process.env.PORT
 const bcrypt = require("bcrypt")
+const jwt =  require("jsonwebtoken")
 const dburl = process.env.URL
 const mongoose = require("mongoose")
 const { UserModel } = require("./Models/Models")
 
+
+app.use(Express.json())
 
 
 async function dbconnect() {
@@ -17,29 +20,51 @@ const dbconnet = await mongoose.connect(dburl)
 }
 dbconnect()
 
-try {
     app.post("/signup",async function Signup(req,res) {
     const {name,email,password} = req.body
-    if(!name || !email || !password) {
+        if(!name || !email || !password) {
         return res.status(404).json({
             message : "details are missing "
         })
-    }
-    const CreateUser = UserModel.create(name,email,password)
+    } 
+    const hashedpassword = await bcrypt.hash(password,10)
+    console.log("the hashed password is",hashedpassword)
+
+    const CreateUser = await UserModel.create({
+        name : name,
+        email : email,
+        password : hashedpassword
+    })
     .then((result) => res.json({
         message : "user create sucessfully"
     }))
 })
-    
-} catch (error) {
-    console.log("error in creating the user")
-}
-/* async function login(req,res) {
-    const {email, password} = req.body
-    if(!email || !password) {
 
+app.post("/signin", async(req,res) => {
+
+    const {email,password} = req.body
+    if(!email || !password){
+        res.json({
+            message : "please enter all the data"
+        })
     }
-} */
+    const user = await UserModel.findOne({email})
+    const verify = await bcrypt.compare(password,user.password)
+    const token = await jwt.sign({id : user._id},"secret")
+    res.cookie("token",token)
+
+        res.json({
+        message : "user found",
+        //user : user,
+        verify : verify,
+        cookie : "cookie sent"
+
+    })
+
+
+    //console.log(token)
+
+})
 
 
 
